6f6e31aa68d9ac1c3f4b72e442d57be05638cd2b
Єфімов Сергій Леонідович <s.efimov@md.mdclinics.com.ua>
2016-10-24 17:05:11 +0300

    correction in output data

--- src/homeTask1/taskOne.java
+++ src/homeTask1/taskOne.java
@@ -6,12 +6,10 @@ public class taskOne {
 	public static void main(String[] args) {
 		int mass[];
 		mass = new int[5];
-		int n = 5;
 		int x = 10000;
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the number: ");
 		int number = input.nextInt();
-		System.out.println("Number is: " + number);
 		for (int i = 0; i < 5; i++) {
 			mass[i] = (number / x);
 			mass[i] = mass[i] % 10;

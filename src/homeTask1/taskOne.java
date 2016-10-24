ee695b8a07ed5f9a2250d26c2df247d7ae1e834f
Єфімов Сергій Леонідович <s.efimov@md.mdclinics.com.ua>
2016-10-24 12:46:11 +0300

    initial commit

---
+++ src/homeTask1/taskOne.java
@@ -0,0 +1,24 @@
+package homeTask1;
+
+import java.util.Scanner;
+
+public class taskOne {
+	public static void main(String[] args) {
+		int mass[];
+		mass = new int[5];
+		int n = 5;
+		int x = 10000;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int number = input.nextInt();
+		System.out.println("Number is: " + number);
+		for (int i = 0; i < 5; i++) {
+			mass[i] = (number / x);
+			mass[i] = mass[i] % 10;
+			x /= 10;
+			System.out.println(mass[i]);
+		}
+		input.close();
+	}
+
+}
